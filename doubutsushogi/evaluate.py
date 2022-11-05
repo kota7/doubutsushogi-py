92fbf123584ae72084e41142ea5c56f1bad01933
Kota Mori <kmori05@gmail.com>
2022-11-05 18:34:11 +0900

    evaluation and state images/html

--- doubutsushogi/evaluate.py
+++ doubutsushogi/evaluate.py
@@ -1,2 +1,138 @@
 # -*- coding: utf-8 -*-
 
+import os
+import bz2
+import shutil
+import hashlib
+import sqlite3
+import sys
+import random
+from urllib.request import urlopen
+from logging import getLogger
+from tempfile import TemporaryDirectory
+
+logger = getLogger(__name__)
+DB_BZ2_URL = "https://github.com/kota7/doubutsushogi-solve/releases/download/v1/doubutsu.db.bz2"
+DB_BZ2_MD5_URL = "https://github.com/kota7/doubutsushogi-solve/releases/download/v1/doubutsu.db.bz2.md5"
+DB_MD5_URL = "https://github.com/kota7/doubutsushogi-solve/releases/download/v1/doubutsu.db.md5"
+DBFILE_ENVNAME = "DOUBUTSUSHOGI_DBFILE"
+DB_SIZE = 1737416
+DB_BZ2_SIZE = 552328
+MAXVALUE = 10000
+
+def _get_md5(filepath, blocksize=2**20):
+    # compute md5 checksum of a file
+    md5 = hashlib.md5()
+    with open(filepath, "rb") as f:
+        while True:
+            data = f.read(blocksize)
+            if len(data) == 0:
+                break
+            md5.update(data)
+    return md5.hexdigest()
+
+def _dbfilepath(filepath: str=None):
+    return filepath or os.environ.get(DBFILE_ENVNAME) or "./doubutsu.db"
+
+def _download_db(filepath: str=None):
+    dbfile = _dbfilepath(filepath)
+    logger.info("DB file: '%s'", dbfile)
+    if os.path.isfile(dbfile):
+        db_md5 = _get_md5(dbfile)
+        logger.info("DB file '%s' exists, md5: '%s'", dbfile, db_md5)
+        with urlopen(DB_MD5_URL) as u:
+            db_md5_target = u.read().decode("utf8")
+        logger.info("Target md5: '%s'", db_md5_target)
+        if db_md5 == db_md5_target:
+            logger.info("md5 matched, no need to download again")
+            return
+        logger.info("md5 not matched, will download again")
+
+    with TemporaryDirectory() as dirname:
+        bz2file = os.path.join(dirname, "tmp.db.bz2")
+        logger.info("Start downloading bz2 file to '%s'", bz2file)
+        with urlopen(DB_BZ2_URL) as u, open(bz2file, "wb") as b:
+            shutil.copyfileobj(u, b)
+        logger.info("Download finished")
+        bz2_md5 = _get_md5(bz2file)
+        with urlopen(DB_BZ2_MD5_URL) as u:
+            bz2_md5_target = u.read().decode("utf8")
+        logger.info("Verifying md5 for bz2 file: local: '%s' vs target: '%s'", bz2_md5, bz2_md5_target)
+        if bz2_md5 != bz2_md5_target:
+            logger.warning("MD5 for bz2 file not matched (local '%s' vs target '%s'), there could have been a problem in downloading",
+                           bz2_md5, bz2_md5_target)
+        
+        logger.info("Decompressing bz2 file to '%s'", dbfile)
+        os.makedirs(os.path.dirname(os.path.abspath(dbfile)), exist_ok=True)
+        with bz2.open(bz2file, "rb") as f, open(dbfile, "wb") as g:
+            shutil.copyfileobj(f, g)
+        logger.info("Decompression finished")
+    # verify md5 again
+    db_md5 = _get_md5(dbfile)
+    with urlopen(DB_MD5_URL) as u:
+        db_md5_target = u.read().decode("utf8")
+    logger.info("Verifying md5 for db file: local: '%s' vs target: '%s'", db_md5, db_md5_target)
+    if db_md5 != db_md5_target:
+        logger.warning("MD5 for bz2 file not matched (local '%s' vs target '%s'), there could have been a problem in downloading",
+                       db_md5, db_md5_target)
+
+
+def evaluate_states(states: list, dbfile: str=None)-> list:
+    # values are from player1's viewpoint
+    if len(states) == 0:
+        return []
+    weights = [3 - 2*s.turn for s in states]  # 1 if first player's turn, -1 if second player's turn
+    indices = tuple(s.normalized_state_index for s in states)
+    _dbfile = _dbfilepath(dbfile)
+    logger.info("Evaluating using the db file at '%s'", _dbfile)
+    if not os.path.isfile(_dbfile):
+        print(f"DB file '{_dbfile}' is not found, will download", file=sys.stderr)
+        _download_db(_dbfile)
+
+    with sqlite3.connect(_dbfile) as conn:
+        c = conn.cursor()
+        q = """
+        SELECT stateIndex, value FROM stateValues WHERE stateIndex IN ({})
+        """.format(",".join("?" * len(indices)))
+        c.execute(q, indices)
+        values = dict(c.fetchall())  # maps stateIndex -> value
+    
+    out = [MAXVALUE if s.winning else values.get(idx) for idx, s in zip(indices, states)]
+    out = [v if v is None else v*w for v, w in zip(out, weights)]
+    return out
+
+def optimal_path(state, depth: int=6, action_only: bool=True, randomize: bool=False, seed: int=None)-> list:
+    # Return an optimal path up to the given depth (length)
+    # If randomize is true, then actions are chosen randomly in case of ties
+    out = []
+    for i in range(depth):
+        actions = state.valid_actions
+        if len(actions) == 0:
+            return out  # edge case where there is no action available
+        next_states = [(a, state.action_result(a)) for a in actions]
+
+        # # check for the winning move
+        # for a, (_, status) in next_states:
+        #     if status == state.turn:
+        #         # can win in one move, return this action and done
+        # if action_only:
+        #         out.append(a)
+        #         return out
+
+        action_values = evaluate_states([s for _, (s, _) in next_states])
+        if randomize:
+            # add tiny random numbers for the randomness
+            random.seed(seed)
+            action_values = [None if v is None else v + 1e-6 * (random.random() - 0.5) for v in action_values]
+
+        # find the "best" actions and its index
+        action_values_no_none = [v for v in action_values if v is not None]
+        optim = max(action_values_no_none) if state.turn == 1 else min(action_values_no_none)
+        optim_idx = action_values.index(optim)
+        action = actions[optim_idx]
+        state = next_states[optim_idx][1][0]
+        if action_only:
+            out.append(action)
+        else:
+            out.append((action, state))
+    return out
--- doubutsushogi/game.py
+++ doubutsushogi/game.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+from logging import getLogger
+from tempfile import TemporaryDirectory
+
+from .images import load_images, load_predefined_images
+logger = getLogger(__name__)
+
 EMPTY = 0
 HIYOKO = 1
 ZOU = 2
@@ -7,7 +15,7 @@ KIRIN = 3
 TORI = 4
 LION = 5
 
-LETTERS = " HZKNLlnkzh"
+LETTERS = ".HZKNLlnkzh"
 
 PRISONER_INDEX = 12   # from index for the prisoners
 
@@ -24,7 +32,7 @@ class Action:
     def _index_to_coord(index):
         if index == PRISONER_INDEX:
             # using captured piece
-            return "**"
+            return "*"
         row, col = int(index/3), index % 3
         row = chr(ord("a") + row)
         col = 1 + col
@@ -34,7 +42,8 @@ class Action:
         piece_name = LETTERS[self.piece]
         coord_from = self._index_to_coord(self.index_from)
         coord_to = self._index_to_coord(self.index_to)
-        return f"{piece_name}{coord_from}-{coord_to}"
+        #return f"{piece_name}{coord_from}-{coord_to}"
+        return f"{piece_name}{coord_to}({coord_from})"
 
     def __repr__(self):
         return f"Action({self.piece}, {self.index_from}, {self.index_to})"
@@ -59,7 +68,7 @@ class State:
         out += "\n"
         out += "Player 1's turn" if self.turn == 1 else "Player 2's turn"
         return out
-    
+
     def __repr__(self):
         return f"State{self._data}"
 
@@ -69,7 +78,7 @@ class State:
     
     def captured(self, player):
         # player 1 or 2
-        i = 12 + (player-1)*2
+        i = 12 + 3*(player-1)
         return self._data[i:i+3]
 
     @property
@@ -106,16 +115,51 @@ class State:
             base *= 3
         return idx
 
+    @staticmethod
+    def from_index(index, turn: int=1):
+        data = [None]*18 + [turn]
+        for i in range(18):
+            data[i] = (index % 11) - 5;
+            index //= 11;
+        for i in range(12, 18):
+            data[i] = index % 3;
+            index //= 3;
+        return State(data)
+
+    @staticmethod
+    def from_text(text):
+        # text in the form
+        # "klz.h..H.ZLK0000001"
+        # dots could be spaces 
+        # '\n' and '\r' are ignored
+        text = text.replace("\n", "").replace("\r", "").replace(" ", ".")
+        if len(text) == 18:
+            text += "1"
+        if len(text) != 19:
+            raise ValueError(f"text length must be 19, but '{text}'", file=sys.stderr)
+        mappings = {
+            ".": EMPTY,
+            "h": -HIYOKO, "z": -ZOU, "k": -KIRIN, "n": -TORI, "l": -LION,
+            "H": HIYOKO, "Z": ZOU, "K": KIRIN, "N": TORI, "L": LION,
+            "0": 0, "1": 1, "2": 2
+        }
+        data = tuple(mappings.get(t) for t in text)
+        for i, (t, d) in enumerate(zip(text, data)):
+            if d is None:
+                raise ValueError(f"Failed to parse '{t}' at position {i}")
+        return State(data)
+
+    @property
     def normalized_state_index(self):
+        if self.turn != 1:
+            return self.flipped.normalized_state_index
         s1 = self.state_index
         s2 = self.mirrored.state_index
         return min(s1, s2)
 
     @staticmethod
     def initial_state():
-        return State([-KIRIN, -LION, -ZOU, EMPTY, -HIYOKO, EMPTY,
-                      EMPTY, HIYOKO, EMPTY, ZOU, LION, KIRIN,
-                      0, 0, 0, 0, 0, 0, 1])
+        return initial_state()
 
     def action_result(self, action)-> tuple:
         # returns a tuple of
@@ -138,14 +182,33 @@ class State:
         # We won't (can't) check for draws (sennichite)
         return _game_status(self)
 
-def _reachable_indices(piece, from_index, turn=1)-> list:
-    #print(piece, from_index, turn)
+    @property
+    def winning(self)-> bool:
+        # Check if the player can win the game in one step
+        for a in self.valid_actions:
+            _, value = self.action_result(a)
+            if value == self.turn:
+                logger.debug("Winning with '%s' from state \n: %s", a, self)
+                return True
+        return False
+
+    def to_image(self, **kwargs):
+        # todo: make arguments more explicit
+        return _state_image(self, **kwargs)
+    
+    def to_html(self, **kwargs):
+        # todo: make arguments more explicit
+        return _state_html(self, **kwargs)
+
+
+def _reachable_indices(piece, from_index)-> list:
+    #print(piece, from_index)
     # returns the indices where the given piece can move
     # from the from_index in one step
-    if turn != 1:
+    if piece < 0:
         # flip the player and find the indices from first player's viewpoint
         # then flip the indices
-        return [11-c for c in _reachable_indices(piece, 11-from_index, turn=1)]
+        return [11-c for c in _reachable_indices(-piece, 11-from_index)]
 
     # we can assume it is the first player's turn
     if not piece in (HIYOKO, ZOU, KIRIN, TORI, LION):
@@ -197,7 +260,7 @@ def _reachable_indices(piece, from_index, turn=1)-> list:
         return out
     else:
         # lion = zou + kirin
-        return _reachable_indices(ZOU, from_index, turn=turn) + _reachable_indices(KIRIN, from_index, turn=turn)
+        return _reachable_indices(ZOU, from_index) + _reachable_indices(KIRIN, from_index)
 
 def valid_actions(state: State)-> list:
     # returns all available actions from the given state
@@ -209,20 +272,26 @@ def valid_actions(state: State)-> list:
             # if state.turn=1, then equivalent to piece <= 0
             # if state.turn=2, then equivalent to piece >= 0
             continue
-        indices = _reachable_indices(piece, i, state.turn)
+        indices = _reachable_indices(piece, i)
         #print(indices)
         for j in indices:
             if piece * state.board[j] <= 0:
                 # not the same sign, i.e. own piece is not on the target cell
-                actions.append(Action(piece, i, j))
+                actions.append(Action(abs(piece), i, j))
     # use captured pieces
     empty_indices = [i for i, piece in enumerate(state.board) if piece == EMPTY]
-    for piece in state.captured(state.turn):
-        if piece <= 0:
+    for k, num in enumerate(state.captured(state.turn)):
+        piece = k + 1  # 0,1,2 -> HIYOKO,ZOU,KIRIN
+        if num <= 0:
             continue
         for j in empty_indices:
             actions.append(Action(piece, PRISONER_INDEX, j))
     return actions
+
+def initial_state():
+    return State([-KIRIN, -LION, -ZOU, EMPTY, -HIYOKO, EMPTY,
+                  EMPTY, HIYOKO, EMPTY, ZOU, LION, KIRIN,
+                  0, 0, 0, 0, 0, 0, 1])
 
 def after_state(state: State, action: Action)-> State:
     # return the state resulted from the given action played on the given state
@@ -238,14 +307,20 @@ def after_state(state: State, action: Action)-> State:
         return State(data)
     # moving a piece on the board
     data = list(state._data)
-    if data[action.index_to] != EMPTY:
+    if data[action.index_to] not in (EMPTY, LION, -LION):
         # this piece will become a prisoner
-        data[11 + abs(data[action.index_to]) + 3*(state.turn-1)] += 1
-    data[action.index_to] = action.piece
-    data[action.index_from] = EMPTY   
+        piece = abs(data[action.index_to])
+        if piece == TORI:
+            piece = HIYOKO
+        data[11 + piece + 3*(state.turn-1)] += 1
+    piece = action.piece
+    index_to = action.index_to
+    if piece == HIYOKO and ((state.turn==1 and action.index_to < 3) or (state.turn==2 and action.index_to >= 9)):
+        piece = TORI  # promotion
+    data[action.index_to] = piece * (3 - 2 * state.turn)
+    data[action.index_from] = EMPTY
     data[-1] = 3 - data[-1]  # turn 1 -> 2, 2 -> 1
     return State(data)
-
 
 def _is_try(state):
     # check if the opponent player has successfully tried
@@ -274,5 +349,80 @@ def _game_status(state: State)-> int:
     if _is_try(state):
         return 3 - state.turn  # won by the opponent
 
+    # check if there is no action available
+    if len(state.valid_actions) == 0:
+        return 3 - state.turn  # won by the opponent
+
     # winning conditions are not satisfied, so game is not finished yet
     return 0
+
+
+def _state_html(state: State, imagename: str=None, imagedir: str=None,
+                bordersize="2px", bordercolor="#424242", bordertype="solid",
+                boardcolor="#d1edf2", imgsize=100,
+                captured_imgsize=40, captured_color="#efefef"):
+    if imagedir is not None:
+        images = load_images(imagedir, as_base64=True)
+    else:
+        images = load_predefined_images(imagename or "emoji1", as_base64=True)
+    
+    def _cell_img(value, **kwargs):
+        if value == EMPTY:
+            return ""
+        piece = ["hiyoko", "zou", "kirin", "tori", "lion"][abs(value)-1]
+        rotate = (value < 0)
+        properties = kwargs.copy()
+        properties["src"] = f"data:image/png;base64,{getattr(images, piece)}"
+        if rotate:
+            properties["style"] = "transform:rotate(180deg);" + properties.get("style", "")
+        return "<img {}>".format(" ".join(f'{k}="{v}"' for k, v in properties.items()))
+
+    board = """
+    <table>
+    <tr><td {tdprop}>{}</td><td {tdprop}>{}</td><td {tdprop}>{}</td></tr>
+    <tr><td {tdprop}>{}</td><td {tdprop}>{}</td><td {tdprop}>{}</td></tr>
+    <tr><td {tdprop}>{}</td><td {tdprop}>{}</td><td {tdprop}>{}</td></tr>
+    <tr><td {tdprop}>{}</td><td {tdprop}>{}</td><td {tdprop}>{}</td></tr>
+    </TABLE>
+    """.format(*[_cell_img(v, width=imgsize) for v in state.board],
+               tdprop=f'style="border: {bordersize} {bordercolor} {bordertype};" bgcolor={boardcolor}')
+    
+    def _captured(values):
+        args = []
+        for i in range(3):
+            args.append(_cell_img(i+1, width=captured_imgsize))
+            args.append(values[i])
+        out ="""
+        <table>
+        <tr><td bgcolor={color}>{} <span>{}</span></td><td bgcolor={color}>{} <span>{}</span></td><td bgcolor={color}>{} <span>{}</span></td></tr>
+        </table>
+        """.format(*args, color=captured_color)
+        return out
+    mover = f"<span>Next mover: {state.turn}</span>"
+    return _captured(state.captured(2)) + board + _captured(state.captured(1)) + mover
+
+def _state_image(state: State, padding=10, threshold=0.99, **kwargs):
+    # we take screenshot of the HTML
+    # this is crude implementation
+    # we may refactor this function
+    from htmlwebshot import WebShot
+    from PIL import Image
+    import numpy as np
+    html = _state_html(state, **kwargs)    
+    shot = WebShot()
+    with TemporaryDirectory() as dirname:
+        imgfile = os.path.join(dirname, "tmp.png")
+        shot.create_pic(html=html, quality=100, output=imgfile)
+        img = Image.open(imgfile).convert("RGB")
+    # crop white rows and cols
+    a = np.asarray(img)
+    white_flag = (a == 255).min(axis=2)  # to do we may better to allow some deviation from white
+    tmp = np.where(white_flag.mean(axis=0) <= threshold)[0]
+    i1 = np.clip(min(tmp) - padding, 0, a.shape[0])
+    i2 = np.clip(max(tmp) + padding, 0, a.shape[0])
+    tmp = np.where(white_flag.mean(axis=1) <= threshold)[0]
+    j1 = np.clip(min(tmp) - padding, 0, a.shape[1])
+    j2 = np.clip(max(tmp) + padding, 0, a.shape[1])
+    img = Image.fromarray(a[j1:j2,i1:i2])
+
+    return img
